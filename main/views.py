a7d0db33c107867edf06393aba45897494ed87b1
Alexander <elektrik.current@gmail.com>
2020-10-07 23:05:47 +0200

    [Refactoring] Moved task update code from view to a separate module

--- main/views.py
+++ main/views.py
@@ -12,6 +12,8 @@ from django.contrib.auth import logout
 
 from .static import *
 
+import main.actions.task_actions as TA
+
 """
 Google OAuth logout
 """
@@ -26,115 +28,6 @@ Backend API methods
 
 
 
-# @todo refactor this postpone method to a separate model method
-
-def _postponeTask(task_id, delay_shift):
-    if delay_shift is not None:
-        delay_shift = int(delay_shift)
-        
-    if delay_shift is None:
-        print("delay shift is not set")      
-        raise Http404("delay shift is not set or not equal to [1,7,30]")
-
-    elif  delay_shift not in [1,7,30]:
-        print("delay shift is not ok, %d" % delay_shift)      
-        raise Http404("delay shift is not set or not equal to [1,7,30]")
-            
-    else:
-        print("delay_shift is ok, %d" % delay_shift)        
-        
-    if task_id is not None:
-        
-        try:
-            qs = Schedule.objects.get(task_id=task_id)
-            qs.next_date = qs.next_date + timedelta(days=delay_shift) 
-            qs.save()
-            task = Task.objects.get(pk = task_id)
-
-            print("saving changelog".encode('utf-8'))
-            qs2 = Changelog.objects.create(task_id = task, action = TASK_LOG_POSTPONE)
-            qs2.save()
-
-        except Schedule.DoesNotExist:
-            raise Http404("Next task date does not exist")
-
-        except DatabaseError as e:
-            print(str(e))
-            
-            raise Http404("Error during save")
-            
-        print(str(qs).encode("utf-8"))
-        print(str(qs2).encode("utf-8"))
-
-    else:
-        raise Http404("Task_id parameter should be set")
-
-
-
-# @todo delay_shift should be loaded from Task properties
-def _completeTask(task_id):
-
-    delay_shift = 7
-
-    if task_id is not None:
-        
-        try:
-            qs = Schedule.objects.get(task_id=task_id)
-            qs.next_date = qs.next_date + timedelta(days=delay_shift) 
-            qs.save()
-            task = Task.objects.get(pk = task_id)
-
-            print("saving changelog".encode('utf-8'))
-            qs2 = Changelog.objects.create(task_id = task, action = TASK_LOG_DONE)
-            qs2.save()
-
-        except Schedule.DoesNotExist:
-            raise Http404("Next task date does not exist")
-
-        except DatabaseError as e:
-            print(str(e))
-            
-            raise Http404("Error during save")
-            
-        print(str(qs).encode("utf-8"))
-        print(str(qs2).encode("utf-8"))
-
-    else:
-        raise Http404("Task_id parameter should be set")
-
-
-
-def _startTask(task_id, start_date):
-        
-    if start_date is None:
-        print("start_date is not set")      
-        raise Http404("start_date is not set ")
-
-    else:
-        print("start_date is ok, %s" % start_date)        
-        
-    if task_id is not None:
-        
-        try:
-            task = Task.objects.get(pk = task_id)
-            
-            qs = Schedule.objects.create(task_id = task, next_date = start_date)
-            
-            qs.save()
-            
-            
-            qs2 = Changelog.objects.create(task_id = task, action = TASK_LOG_START)
-            qs2.save()
-        except DatabaseError as e:
-            print(str(e).encode("utf-8"))
-            
-            raise Http404("Error during save")
-            
-        print(str(qs).encode("utf-8"))
-
-    else:
-        raise Http404("Task_id parameter should be set")
-
 
 
 class TaskPostpone(generics.ListAPIView):
@@ -146,7 +39,7 @@ class TaskPostpone(generics.ListAPIView):
         task_id = self.request.query_params.get('task_id', None)
         delay_shift = self.request.query_params.get('delay_shift', None)
 
-        _postponeTask(task_id, delay_shift) 
+        TA._postponeTask(task_id, delay_shift) 
                     
         queryset = Schedule.objects.filter(task_id=task_id).all()
 
@@ -206,12 +99,12 @@ def index(request):
 def task_start(request, task_id):
     start_date = request.POST.get('start_date', None)
     
-    _startTask(task_id, start_date)
+    TA._startTask(task_id, start_date)
         
     return HttpResponse("Task %d now has start date %s, <a href='javascript:history.go(-1)'>Go back</a>" % (task_id, start_date))
 
 def task_done(request, task_id):
-    _completeTask(task_id)
+    TA._completeTask(task_id)
     return HttpResponse("Task %d marked as done, <a href='javascript:history.go(-1)'>Go back</a>" % task_id)
 
 """
@@ -220,7 +113,7 @@ UI AJAX Methods
 
 def ajax_task_done(request):
     task_id = int(request.GET.get('task_id', None))
-    _completeTask(task_id)
+    TA._completeTask(task_id)
 
     data = {
         'result': ("Task %d marked as done" % task_id)
@@ -231,7 +124,7 @@ def ajax_task_done(request):
 def ajax_task_postpone(request):
     task_id = int(request.GET.get('task_id', None))
     delay_shift = int(request.GET.get('delay_shift', None))
-    _postponeTask(task_id, delay_shift)
+    TA._postponeTask(task_id, delay_shift)
     data = {
         'result': ("Task %d postponed for %d days" % (task_id, delay_shift))
     }
@@ -243,7 +136,7 @@ def ajax_task_postpone(request):
 
 
 def task_postpone(request, task_id, delay_shift):
-    _postponeTask(task_id, delay_shift)
+    TA._postponeTask(task_id, delay_shift)
     return HttpResponse("Task %d postponed for %d days, <a href='javascript:history.go(-1)'>Go back</a>" % (task_id, delay_shift))
 
 def task(request, task_id):
