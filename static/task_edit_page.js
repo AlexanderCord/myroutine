dc982376b8a0afc50cdce47a659138a501088644
Alexander <elektrik.current@gmail.com>
2021-02-21 05:02:49 +0100

    [UI] Small refactoring

---
+++ static/task_edit_page.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+ 
+   if(startDatepicker) {
+     $('.datepicker').datepicker({
+       dateFormat: 'yy-mm-dd',
+       firstDay: 1,
+     });
+   }
+
+   $('#id_period').on('change', function() {
+     // custom period
+     if(this.value == 2) {
+        $('#id_period_data').parent().parent().show();
+        $('#id_period_data').attr('required', 'required');
+        $('#id_period_data').attr('min', '1');
+        
+
+        
+     } else {
+        $('#id_period_data').parent().parent().hide();
+        $('#id_period_data').removeAttr('required');
+        $('#id_period_data').removeAttr('min');
+        
+     }
+   });
+   $('#id_period').trigger('change');
+ });    
