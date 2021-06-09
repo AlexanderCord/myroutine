5b25054d4940ba1b9f12ea093e00378745f773d3
Alexander <elektrik.current@gmail.com>
2021-06-09 05:07:38 +0200

    Unit test draft

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Task
+
+
+class TaskModelTests(TestCase):
+
+    def test_task_create(self):
+        self.assertIs(True, True)
+        print("Test ok")
